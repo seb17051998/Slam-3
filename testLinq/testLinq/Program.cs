eaee94f49f9631486a54b137331779139e67496e
BUNEL Sébastien <bunelsebastien95@gmail.com>
2018-03-28 14:48:42 +0200

    Add files via upload

---
+++ testLinq/testLinq/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> liste = new List<int> { 4, 6, 1, 9, 5, 15, 8, 3, 20, 7 };
+            
+
+            
+            IEnumerable<int> requeteTriee = from i in liste where i>5 orderby i select i;
+            foreach(int i in requeteTriee)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
